19f66c13d58757d2568a009f4f87cfdda3c8d14f
Jeremy han <38609717+jeremyhann@users.noreply.github.com>
2024-05-23 13:42:58 +0800

    update title更新

--- src/components/IPTag.vue
+++ src/components/IPTag.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="flex items-center cursor-pointer">
+    <div class="flex items-center cursor-pointer" title="Copy IP address">
         <div class="p-3 rounded-full " :class="ip&&ip.length?'bg-primary':'bg-[#EBF3FF]'">
             <Link :style="{fill:ip&&ip.length?'#fff':'rgba(26, 26, 26, 0.35)'}"/>
         </div>
-        <div v-if="ip&&ip.length" class="bg-white border rounded-md px-2 leading-[38px] ml-2 cursor-pointer hover:bg-[#EEE]" title="Copy IP address">
+        <div v-if="ip&&ip.length" class="bg-white border rounded-md px-2 leading-[38px] ml-2 cursor-pointer hover:bg-[#EEE]" >
             <template v-for="(item,index) in ip.split('.')">
                 <span>{{ item }}</span>
                 <i v-if="index<3" class="opacity-35 mr-[2px]">·</i>
